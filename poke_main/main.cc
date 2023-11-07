5d0223501bf6e4ad4fa08d4dfdaf858436c3ec13
dejavuapt <thepoli.b@gmail.com>
2023-11-07 12:02:37 +0500

    return to Python3

--- poke_main/main.cc
+++
@@ -1,8 +0,0 @@
-#include <drogon/drogon.h>
-int main() {
-    //Set HTTP listener address and port
-    drogon::app().addListener("127.0.0.1", 5555);
-    LOG_INFO << "Server running on 127.0.0.1";
-    drogon::app().run();
-    return 0;
-}
